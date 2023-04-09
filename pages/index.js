924b4d694b00d13a5590670dd7b7f1c246114371
stevenleisu <stevenleisu@tencent.com>
2023-04-09 22:02:41 +0800

    support many style

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,85 @@ export default function Home() {
   const [seed] = useState(seeds[Math.floor(Math.random() * seeds.length)]);
   const [initialPrompt] = useState(seed.prompt);
   const [scribble, setScribble] = useState(null);
+  var [picStyle, setPicStyleStr] = useState(null);
+  var [picArt, setPicArtStr] = useState(null);
+  var [picComposition, setPicCompositionStr] = useState(null);
+  var [picTech, setPicTechStr] = useState(null);
+  var [picAtmosphere, setPicAtmosphereStr] = useState(null);
+  var [picResolution, setPicResolutionStr] = useState(null);
+  
+  
+// 1.风格
+// Studio Ghibli（吉卜力工作室）
+// Pixar（皮克斯）
+// Cyberpunk（赛博朋克）
+// Wasteland Punk（荒地朋克）
+// Illustration（插图）
+// Realism（现实主义）
+// Landscape（景观）
+// Surrealism（超现实主义）
+// Watercolor painting（水彩画）
+// Visual impact（视觉冲击）
+// Ukiyoe（浮世绘）
+// Neo-realism（新现实主义）
+// Post-impressionism（后印象派）
+// Architectural design（建筑设计）
+// Watercolor（水彩画）
+// Poster style（海报风格）
+// Ink style（水墨风格）
+
+// 2.著名艺术家
+// Vincent van Gogh（文森特-凡高）
+// Pablo Picasso（巴勃罗-毕加索）
+// Wassily Kandinsky（瓦西里·康定斯基）
+// Leonardo da Vinci（莱昂纳多-达芬奇）
+// Max Ernst（马克斯-恩斯特）
+// Salvador Dali（萨尔瓦多-达利）
+// Norman Rockwell（诺曼-洛克威尔）
+// Jackson Pollock（杰克逊-波洛克）
+// Mattias Adolfsso（马蒂亚斯-阿道夫索）
+
+// 3.构图
+// close up（特写）
+// full body（全身）
+// portrait（肖像）
+// symmetrical（对称的）
+// wide view（广视角）
+// bird view（鸟瞰）
+// top view（俯视）
+// up view（仰视）
+// front view（正面图）
+// headshot（大头照）
+// ultrawide shot（超广角拍摄）
+// extreme closeup（极端特写）
+// macro shot（微距拍摄）
+
+// 4.技术效果
+// epic detail（史诗般的细节）
+// dramatic contrast（戏剧性的对比）
+// octane render（OC渲染器）
+// unreal engine 5（虚幻引擎5）
+// vray（vray渲染器）
+// dof（透明度）
+// 4k, 8k, 16k, 32k（分辨率）
+// HD（高清）
+
+// 5.氛围
+// happy（快乐）
+// excited（兴奋）
+// angry（愤怒）
+// sad（悲伤）
+// disgusted（厌恶）
+// surprised（惊讶）
+// hopeful（充满希望）
+// anxious（焦虑）
+// elated（高兴的）
+// fearful（恐惧的）
+// hateful（可恨）
+// moody（喜怒无常）
+// dark（黑暗）
+// brutal（粗暴的）
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -34,10 +113,41 @@ export default function Home() {
     // track submissions so we can show a spinner while waiting for the next prediction to be created
     setSubmissionCount(submissionCount + 1);
 
-    const prompt = e.target.prompt.value
+    let prompt = e.target.prompt.value
       .split(/\s+/)
       .map((word) => (naughtyWords.en.includes(word) ? "something" : word))
       .join(" ");
+
+    if(picStyle && picStyle.length>0)
+    {
+      prompt += "," + picStyle;
+    }
+
+    if(picArt && picArt.length>0)
+    {
+      prompt += "," + picArt;
+    }
+
+    if(picComposition && picComposition.length>0)
+    {
+      prompt += "," + picComposition;
+    }
+
+    if(picTech && picTech.length>0)
+    {
+      prompt += "," + picTech;
+    }
+
+    if(picAtmosphere && picAtmosphere.length>0)
+    {
+      prompt += "," + picAtmosphere;
+    }
+
+    if(picResolution && picResolution.length>0)
+    {
+      prompt += "," + picResolution;
+    }
+    
 
     setError(null);
     setIsProcessing(true);
@@ -190,7 +300,25 @@ function _toPixels (canvas) {
       video.srcObject = stream; }, 500);    
   };
 
+  const handleStyleChange = (event) => {
+    picStyle = event.target.value;
+  }
+  const handleArtChange = (event) => {
+    picArt = event.target.value;
+  }
+  const handleCompositionChange = (event) => {
+    picComposition = event.target.value;
+  }
+  const handleTechChange = (event) => {
+    picTech = event.target.value;
+  }
+  const handleAtmosphereChange = (event) => {
+    picAtmosphere = event.target.value;
+  }
 
+  const handleResolutionChange = (event) => {
+    picResolution = event.target.value;
+  }
 
 
   return (
@@ -220,7 +348,7 @@ function _toPixels (canvas) {
           {photoMode == true && 
             <div>
               <video id="video" width="320" height="240" autoPlay></video>
-              <button id="click-photo" onClick={takePicture} >Take Photo</button>
+              <button id="click-photo" onClick={takePicture} >拍照</button>
               <div id="dataurl-container">
                 <canvas id="canvas" width="320" height="240"></canvas>
                 <canvas id="contrastcanvas" width="320" height="240"></canvas>
@@ -228,7 +356,7 @@ function _toPixels (canvas) {
                 <textarea id="dataurl" readOnly></textarea>
               </div>
               <div id="contrast-container"></div>
-              <button onClick={closeCamera}>Reset</button>
+              <button onClick={closeCamera}>重置并关闭摄像头</button>
             </div>
             
           }
@@ -241,11 +369,113 @@ function _toPixels (canvas) {
               scribbleExists={scribbleExists}
               setScribbleExists={setScribbleExists}
               />   
-              <button onClick={openCamera}>Open camera</button>
+              <button onClick={openCamera}>打开摄像头</button>
             </div>
           }
-          
-          
+        
+          <div>
+            <div>
+              <br></br>
+              风格：
+              <select id="dropdown" onChange={handleStyleChange}>
+                <option value="">N/A</option>
+                <option value="Studio Ghibli">吉卜力工作室</option>
+                <option value="Pixar">皮克斯</option>
+                <option value="Cyberpunk">赛博朋克</option>
+                <option value="Wasteland Punk">荒地朋克</option>
+                <option value="Illustration">插图</option>
+                <option value="Realism">现实主义</option>
+                <option value="Landscape">景观</option>
+                <option value="Surrealism">超现实主义</option>
+                <option value="Watercolor painting">水彩画</option>
+                <option value="Visual impact">视觉冲击</option>
+                <option value="Ukiyoe">浮世绘</option>
+                <option value="Neo-realism">新现实主义</option>
+                <option value="Post-impressionism">后印象派</option>
+                <option value="Architectural design">建筑设计</option>
+                <option value="Watercolor">水彩画</option>
+                <option value="Poster style">海报风格</option>
+                <option value="Ink style">水墨风格</option>
+              </select>
+              <br></br>
+
+              艺术家：
+              <select id="dropdown" onChange={handleArtChange}>
+                <option value="">N/A</option>
+                <option value="Vincent van Gogh">文森特-凡高</option>
+                <option value="Pablo Picasso">巴勃罗-毕加索</option>
+                <option value="Wassily Kandinsky">瓦西里·康定斯基</option>
+                <option value="Leonardo da Vinci">莱昂纳多-达芬奇</option>
+                <option value="Max Ernst">马克斯-恩斯特</option>
+                <option value="Salvador Dali">萨尔瓦多-达利</option>
+                <option value="Norman Rockwell">诺曼-洛克威尔</option>
+                <option value="Jackson Pollock">杰克逊-波洛克</option>
+                <option value="Mattias Adolfsso">马蒂亚斯-阿道夫索</option>
+              </select>
+
+              <br></br>
+              构图：
+              <select id="dropdown" onChange={handleCompositionChange}>
+                <option value="">N/A</option>
+                <option value="close up">特写</option>
+                <option value="full body">全身</option>
+                <option value="portrait">肖像</option>
+                <option value="symmetrical">对称的</option>
+                <option value="wide view">广视角</option>
+                <option value="bird view">鸟瞰</option>
+                <option value="top view">俯视</option>
+                <option value="up view">仰视</option>
+                <option value="front view">正面图</option>
+                <option value="headshot">大头照</option>
+                <option value="ultrawide shot">超广角拍摄</option>
+                <option value="extreme closeup">极端特写</option>
+                <option value="macro shot">微距拍摄</option>
+              </select>
+
+              <br></br>
+              技术效果：
+              <select id="dropdown" onChange={handleTechChange}>
+                <option value="">N/A</option>
+                <option value="epic detail">史诗般的细节</option>
+                <option value="dramatic contrast">戏剧性的对比</option>
+                <option value="octane render">OC渲染器</option>
+                <option value="unreal engine 5">虚幻引擎5</option>
+                <option value="vray">vray渲染器</option>
+                <option value="dof">透明度</option>
+              </select>
+
+              <br></br>
+              氛围：
+              <select id="dropdown" onChange={handleAtmosphereChange}>
+                <option value="">N/A</option>
+                <option value="happy">快乐</option>
+                <option value="excited">兴奋</option>
+                <option value="angry">愤怒</option>
+                <option value="sad">悲伤</option>
+                <option value="disgusted">厌恶</option>
+                <option value="surprised">惊讶</option>
+                <option value="hopeful">充满希望</option>
+                <option value="anxious">焦虑</option>
+                <option value="elated">高兴的</option>
+                <option value="fearful">恐惧的</option>
+                <option value="hateful">可恨</option>
+                <option value="moody">喜怒无常</option>
+                <option value="dark">黑暗</option>
+                <option value="brutal">粗暴的</option>
+              </select>
+
+              <br></br>
+              清晰度：
+              <select id="dropdown" onChange={handleResolutionChange}>
+                <option value="">N/A</option>
+                <option value="HD">高清</option>
+                <option value="4k">4k</option>
+                <option value="8k">8k</option>
+                <option value="16k">16k</option>
+                <option value="32k">32k</option>
+              </select>
+            </div>
+          </div>
 
           <PromptForm
             initialPrompt={initialPrompt}
